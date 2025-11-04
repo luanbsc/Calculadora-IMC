ce5aec6feeb40347fc1c3a0658d7655862051c41
luanbsc <luanbscosta@hotmail.com>
2025-11-04 16:17:40 -0300

    feat: Adiciona gráfico de histórico de medições

--- Calculadora IMC/ViewModels/ChartViewModel.cs
+++ Calculadora IMC/ViewModels/ChartViewModel.cs
@@ -1,20 +1,48 @@
 ﻿using Calculadora_IMC.Core;
 using Calculadora_IMC.Models;
-using System;
-using System.Collections.Generic;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculadora_IMC.ViewModels
 {
     public class ChartViewModel : ViewModelBase
     {
-        public ObservableCollection<Medicao> _medicoes;
-        public ChartViewModel(ObservableCollection<Medicao> Medicoes)
+        public ObservableCollection<Medicao> Medicoes { get; set; }
+        public ISeries[] Series { get; set; }
+        public Axis[] XAxes { get; set; }
+
+        public ChartViewModel(ObservableCollection<Medicao> medicoes)
         {
-            _medicoes = Medicoes;
+            Medicoes = medicoes;
+
+            double[] pesos = Medicoes.Select(medicao => medicao.Peso).ToArray();
+            double[] imcs = Medicoes.Select(medicao => Math.Round(medicao.IMC, 2)).ToArray();
+            string[] datas = Medicoes.Select(medicao => medicao.Data.ToString("dd/MM HH:mm")).ToArray();
+
+            Series =
+            [
+                new ColumnSeries<double>
+                {
+                    Values = pesos,
+                    Name = "Peso (kg)"
+                },
+                new LineSeries<double>
+                {
+                    Values = imcs,
+                    Name = "IMC",
+                    GeometrySize = 10, // bolinhas nos pontos
+                    LineSmoothness = 0 // linha reta
+                }
+            ];
+
+            XAxes =
+            [
+                new Axis
+                {
+                    Labels = datas
+                }
+            ];
         }
     }
 }
